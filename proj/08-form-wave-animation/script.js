1b4d7def9aa0d024f6349bcc3b16f9211f4d32ea
YKIM <96836324+CKIMM7@users.noreply.github.com>
2022-07-07 10:03:57 +0100

    Add files via upload

---
+++ proj/08-form-wave-animation/script.js
@@ -0,0 +1,10 @@
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    console.log(
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 6000}ms">${letter}</span>`)
+        .join('')
+        )
+})
